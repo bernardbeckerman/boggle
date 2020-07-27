1cd719c0f6839d583705ca02746a127103b52f34
Bernard Beckerman <7953022+bernardbeckerman@users.noreply.github.com>
2020-07-27 07:40:52 -0400

    adds setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='boggle',
+      version='0.0',
+      description='boggle game funtimes',
+      author='Bernard Beckerman',
+      author_email='bernard.beckerman@gmail.com',
+      url='https://github.com/bernardbeckerman/',
+      #packages=['distutils', 'distutils.command'],
+     )
